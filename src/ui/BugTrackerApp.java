c2bdd9bda11f9da0e641d20aa69be65c503fe386
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-15 18:06:01 -0500

    created BugTrackerApp
    the main class to run the application

---
+++ src/ui/BugTrackerApp.java
@@ -0,0 +1,19 @@
+/**
+ * @Author Jon
+ * @Version 0.1
+ */
+
+package ui;
+
+public class BugTrackerApp {
+    public static void main(String args[]){
+
+        // check for existence of text file
+        // better to fail early than later
+
+    }
+
+    private static void printMenu(){
+
+    }
+}
