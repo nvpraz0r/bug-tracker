6337078d2533d48ce10600b9f0038061c038fc68
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-15 18:16:56 -0500

    Added menu function

--- src/ui/BugTrackerApp.java
+++ src/ui/BugTrackerApp.java
@@ -10,10 +10,23 @@ public class BugTrackerApp {
 
         // check for existence of text file
         // better to fail early than later
+        // add try block to create text file
+
+        // print command menu
+        System.out.println("Bug Tracker");
+
 
     }
 
     private static void printMenu(){
-
+        System.out.println("List of commands:");
+        System.out.println("L - List all bugs");
+        //add functionality to this
+        System.out.println("A - List active bugs");
+        System.out.println("I - Change bug to inactive");
+        System.out.println("C - Create a new entry:");
+        System.out.println("D - Delete an entry:");
+        System.out.println("H - Help:");
+        System.out.println("E - Exit");
     }
 }
