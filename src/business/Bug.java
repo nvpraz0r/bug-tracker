7e6cdbff07aba186c1a37149a239bce07bc34f3f
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-16 00:10:44 -0500

    Create Bug POJO

---
+++ src/business/Bug.java
@@ -0,0 +1,17 @@
+/**
+ * @Author Jon
+ */
+
+package business;
+
+/**
+ * POJO
+ */
+public class Bug {
+
+    private double id;
+    private String name;
+    private String description;
+    private boolean status;
+
+}
